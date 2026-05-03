45db30978348da4c1ef5fc1bb29582f409629e10
pura55 <nekotonaito2@gmail.com>
2026-05-03 15:53:36 +0900

    Change the location where images are loaded.(画像をロードする場所を変更)

--- project/Source/ImageLoader.cpp
+++ project/Source/ImageLoader.cpp
@@ -3,10 +3,13 @@
 ImageLoader::ImageLoader()
 {
 	DontDestroyOnSceneChange();
+	commandWindow = LoadGraph("Data/image/CommandWindowSmall.png");
+	assert(commandWindow > 0);
 }
 
 ImageLoader::~ImageLoader()
 {
+	DeleteGraph(commandWindow);
 }
 
 void ImageLoader::Update()
--- project/Source/ImageLoader.h
+++ project/Source/ImageLoader.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "../Library/GameObject.h"
+#include <cassert>
 
+/// <summary>
+/// イメージローダー
+/// 
+/// 画像を読み込んで、他のクラスに提供するクラス
+/// </summary>
 class ImageLoader : public GameObject
 {
 public:
@@ -8,8 +14,15 @@ public:
 	~ImageLoader();
 	void Update() override;
 
+/// <summary>
+/// Getter関数
+/// </summary>
 public:
 	int GetCommandWindow() const{ return commandWindow; }
+
+/// <summary>
+/// 読み込んだ画像を格納する変数
+/// </summary>
 private:
-	int commandWindow = LoadGraph("Data/image/CommandWindow.png");
+	int commandWindow;
 };
