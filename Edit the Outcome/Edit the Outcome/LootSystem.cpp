92c0e34f51648c87e3b77e8c2a7d0796f2fe3c3d
pura55 <nekotonaito2@gmail.com>
2026-07-17 15:34:06 +0900

    ルートシステムの処理追加
    /// 修正 ///
    ・LootSystem.hpp/cpp：獲得するルートアイテムを決める入力処理を追加（後日、追加・修正予定）

--- Edit the Outcome/Edit the Outcome/LootSystem.cpp
+++ Edit the Outcome/Edit the Outcome/LootSystem.cpp
@@ -10,10 +10,13 @@ void LootSystem::update()
 	switch (m_lootState)
 	{
 	case LootState::Init:
+		LootInit();
 		break;
 	case LootState::Selecting:
+		LootSelecting();
 		break;
 	case LootState::Finished:
+		LootFinished();
 		break;
 	}
 
@@ -28,4 +31,105 @@ void LootSystem::SetLootData(std::vector<CommandData>& commandData, std::vector<
 	// データの参照を格納
 	m_commandData = commandData;
 	m_playerData = playerData;
+}
+
+void LootSystem::LootInit()
+{
+	DecideKindOfItem();
+
+	m_selectIndex = 0;
+	m_checkIndex = 0;
+	m_needAcquireCheck = false;
+}
+
+void LootSystem::LootSelecting()
+{
+	if (not m_needAcquireCheck)
+	{
+		AcquireItem();
+	}
+	else
+	{
+		if (CheckAcquisition())
+		{
+			m_lootState = LootState::Finished;
+			return;
+		}
+	}
+}
+
+void LootSystem::LootFinished()
+{
+}
+
+void LootSystem::DecideKindOfItem()
+{
+
+}
+
+void LootSystem::AcquireItem()
+{
+	if (KeyA.down())
+	{
+		m_selectIndex -= 1;
+
+		if (m_selectIndex < 0)
+		{
+			m_selectIndex = 0;
+		}
+	}
+
+	if (KeyD.down())
+	{
+		m_selectIndex += 1;
+
+		if (m_selectIndex >= m_maxSelectIndex)
+		{
+			m_selectIndex = m_maxSelectIndex;
+		}
+	}
+
+	if (KeySpace.down())
+	{
+		m_needAcquireCheck = true;
+	}
+}
+
+bool LootSystem::CheckAcquisition()
+{
+	if (KeyA.down())
+	{
+		m_checkIndex -= 1;
+
+		if (m_checkIndex < 0)
+		{
+			m_checkIndex = 0;
+		}
+	}
+
+	if (KeyD.down())
+	{
+		m_checkIndex += 1;
+
+		if (m_checkIndex >= m_maxCheckIndex)
+		{
+			m_checkIndex = m_maxCheckIndex;
+		}
+	}
+
+	if (KeySpace.down())
+	{
+		switch (m_checkIndex)
+		{
+		case 0:
+			return true;
+			break;
+		case 1:
+			m_needAcquireCheck = false;
+			return false;
+			break;
+		}
+	}
+
+	return false;
 }
--- Edit the Outcome/Edit the Outcome/LootSystem.hpp
+++ Edit the Outcome/Edit the Outcome/LootSystem.hpp
@@ -5,10 +5,12 @@
 
 enum LootState
 {
-	Init,
-	Selecting,
-	Finished
+	Init,      // 初期化
+	Selecting, // 選択
+	Finished   // 終了
 };
+
+/// <summry>
 
 class LootSystem
 {
@@ -19,15 +21,43 @@ public:
 	// ルートに関わるデータを格納する関数
 	void SetLootData(std::vector<CommandData>& commandData, std::vector<PlayerProgressData>& playerData);
 
+	// 初期化の処理を行う関数
+	void LootInit();
+
+	// ルートアイテム選択状態の関数
+	void LootSelecting();
+
+	// ルートアイテム選択終了の処理を行う関数
+	void LootFinished();
+
+	// アイテムの種類を決定する関数
+	void DecideKindOfItem();
+
+	// アイテムを獲得する関数
+	void AcquireItem();
+
+	// アイテム獲得を確認する関数
+	bool CheckAcquisition();
+
 	// ルートアイテムを選択したかどうかのフラグを返す関数
 	bool GetLootItemSelected() { return m_lootItemSelected; }
 
 private:
-	LootState m_lootState;
+	LootState m_lootState{ LootState::Init }; // ルートステート
 
-	bool m_lootItemSelected{ false };
+	int32 m_maxSelectIndex{ 0 }; //選択指数の最大値
 
-	std::vector<CommandData> m_commandData;
+	int32 m_selectIndex{ 0 }; // 選択指数
 
-	std::vector<PlayerProgressData> m_playerData;
+	int32 m_maxCheckIndex{ 1 }; // 確認指数の最大値
+
+	int32 m_checkIndex{ 0 }; // 確認指数
+
+	bool m_lootItemSelected{ false }; // アイテムを選択したかどうかのフラグ
+
+	bool m_needAcquireCheck{ false }; // アイテムを獲得するかどうかを確認するフラグ(true: 確認する, false: 確認しない）
+
+	std::vector<CommandData> m_commandData; // コマンドデータ
+
+	std::vector<PlayerProgressData> m_playerData; // プレイヤーデータ
 };
